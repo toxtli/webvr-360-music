db7e3f87a3960083a1659c6683157b06a4daf784
toxtli <ctoxtli@gmail.com>
2020-02-29 19:55:08 -0500

    importer updated

--- server/add_from_youtube.sh
+++ server/add_from_youtube.sh
@@ -1,6 +1,6 @@
-rm $1.mp3
+rm songs/$1.mp3
 rm -fr songs/$1
 rm -fr ../audio/$1
-youtube-dl -x --audio-format mp3 -o $1.mp3 $2
-spleeter separate -i $1.mp3 -p spleeter:5stems -o songs/$1
+youtube-dl -x --audio-format mp3 -o songs/$1.mp3 $2
+spleeter separate -i songs/$1.mp3 -p spleeter:5stems -o songs/$1
 python split.py songs/$1 ../audio/$1
