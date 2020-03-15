cadb92d235867de7a7a54c911200ec47e9a15916
toxtli <ctoxtli@gmail.com>
2020-03-14 23:26:40 -0400

    ignore and files deleted

--- server/add_from_youtube.sh
+++
@@ -1,6 +0,0 @@
-rm songs/$1.mp3
-rm -fr songs/$1
-rm -fr ../audio/$1
-youtube-dl -x --audio-format mp3 -o songs/$1.mp3 $2
-spleeter separate -i songs/$1.mp3 -p spleeter:5stems -o songs
-python split.py songs/$1 ../audio/$1
