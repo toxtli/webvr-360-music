86c2ab42e02b65b155bf1555e9e8bce644072874
toxtli <ctoxtli@gmail.com>
2020-04-14 22:54:05 -0400

    url formating

--- js/index.js
+++ js/index.js
@@ -74,6 +74,37 @@ document.getElementById('toggleSearch').addEventListener('click', () => {
 	}
 });
 
+document.getElementById('enable-motion').addEventListener('click', () => {
+    // feature detect
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            window.addEventListener('devicemotion', () => {});
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+});
+
+document.getElementById('enable-orientation').addEventListener('click', () => {
+    // feature detect
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            window.addEventListener('deviceorientation', () => {});
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+});
+
+
 (function() {
 	if (!location.hash.substr(1)) {
 		var select = document.getElementById('songsList');
