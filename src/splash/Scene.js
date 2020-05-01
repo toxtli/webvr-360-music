d91b2d82baadc3edd10a3edd542c90660ca5bcc8
toxtli <ctoxtli@gmail.com>
2020-05-01 08:27:56 -0400

    camera fixes

--- src/splash/Scene.js
+++ src/splash/Scene.js
@@ -46,7 +46,7 @@ export class SplashScene {
 
         const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.001, 10000)
-        camera.position.set(0, 0, 0)
+        camera.position.set(0, -10, 0)
         camera.lookAt(new THREE.Vector3(0, 0, 0))
         const renderer = new THREE.WebGLRenderer({canvas})
         renderer.setClearColor(0x00, 1)
